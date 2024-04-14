bd1d647a7477dcdc82441a2a196777b0e3a25ba8
Palash-Halder <palashhalder.ju@gmail.com>
2024-04-14 10:05:19 +0600

    27-8 random alphabet game recap

--- 27-8 (Recap) Random Alphabet And Highlight Selected Alphabet/scripts/alpha-clash.js
+++ 27-8 (Recap) Random Alphabet And Highlight Selected Alphabet/scripts/alpha-clash.js
@@ -24,14 +24,8 @@ function continueGame(){
     // step 1: generate a random alphabet
     const alphabet = getRandomAlphabet();  
     console.log('Your random alphabet', alphabet);
-
-    // set randomly generated alphabet to the screen (show it)
-    const currentAlphabetElement = document.getElementById('current-alphabet');
-    currentAlphabetElement.innerText = alphabet;
-
-    setBackgroundColorById(alphabet); 
 }
- 
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
